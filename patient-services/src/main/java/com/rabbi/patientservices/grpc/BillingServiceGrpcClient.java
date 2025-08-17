1d59257137b292fce4a8a51d2c10a26ab6ae2224
Fuad Rabbi <fuadhrabbi66@gmail.com>
2025-08-17 14:04:20 -0400

    grpc set up for patient service

---
+++ patient-services/src/main/java/com/rabbi/patientservices/grpc/BillingServiceGrpcClient.java
@@ -0,0 +1,39 @@
+package com.rabbi.patientservices.grpc;
+
+
+import billing.BillingRequest;
+import billing.BillingResponse;
+import billing.BillingServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class BillingServiceGrpcClient {
+    private final BillingServiceGrpc.BillingServiceBlockingStub blockingStub;
+
+    public BillingServiceGrpcClient(
+            @Value("${billing.service.address:localhost}") String serverAddress,
+            @Value("${billing.service.grpc.port:9001}") int serverPort) {
+
+        log.info("Connecting to Billing Service GRPC service at {}:{}",
+                serverAddress, serverPort);
+        ManagedChannel channel = ManagedChannelBuilder.forAddress(serverAddress, serverPort)
+                .usePlaintext().build();
+        blockingStub = BillingServiceGrpc.newBlockingStub(channel);
+    }
+
+    public BillingResponse createBillingAccount(String patientId, String name, String email) {
+        BillingRequest request = BillingRequest.newBuilder().setPatientId(patientId)
+                .setName(name).setEmail(email).build();
+        BillingResponse response = blockingStub.createBillingAccount(request);
+        log.info("Received response from billing service via GRPC: {}", response);
+        return response;
+    }
+}
+//This BillingServiceGrpcClient is a Spring service that connects the patient service to the billing service via gRPC.
+// It creates a channel to the billing server (default: localhost:9001) and uses a blocking stub to send requests.
+// The createBillingAccount method builds a request with patient details, sends it to the billing service, logs the response, and returns it.
