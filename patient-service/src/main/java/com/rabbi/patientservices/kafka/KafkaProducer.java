f4283aacde11a4ee4245cc1838972ef87758dcd3
Fuad Rabbi <fuadhrabbi66@gmail.com>
2025-08-21 20:24:41 -0400

    set up KafkaProducer

--- patient-service/src/main/java/com/rabbi/patientservices/kafka/KafkaProducer.java
+++ patient-service/src/main/java/com/rabbi/patientservices/kafka/KafkaProducer.java
@@ -1,7 +1,9 @@
 package com.rabbi.patientservices.kafka;
 
+import com.rabbi.patientservices.model.Patient;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import patient.events.PatientEvent;
 
 @Service
 public class KafkaProducer {
@@ -11,5 +13,14 @@ public class KafkaProducer {
 
     public KafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendEvent(Patient patient) {
+        PatientEvent event = PatientEvent.newBuilder()
+                .setPatientId(patient.getId().toString())
+                .setName(patient.getName())
+                .setEmail(patient.getEmail())
+                .setEventType("PATIENT_CREATED")
+                .build();
     }
 }
