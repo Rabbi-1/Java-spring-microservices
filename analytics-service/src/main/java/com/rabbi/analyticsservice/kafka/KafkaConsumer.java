07b243260a7715f1650ba63227e9bd8ff8c5aaee
Fuad Rabbi <fuadhrabbi66@gmail.com>
2025-08-27 21:08:06 -0400

    created kafkaconsumer to perform any bussiness relate topics

--- analytics-service/src/main/java/com/rabbi/analyticsservice/kafka/KafkaConsumer.java
+++ analytics-service/src/main/java/com/rabbi/analyticsservice/kafka/KafkaConsumer.java
@@ -3,7 +3,6 @@ package com.rabbi.analyticsservice.kafka;
 import com.google.protobuf.InvalidProtocolBufferException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import patient.events.PatientEvent;
