d9fa96ca9df053ee9610717ce8142bbdd3148b9d
Fuad Rabbi <fuadhrabbi66@gmail.com>
2025-09-07 21:57:56 -0400

    DTO created

---
+++ auth-service/src/main/java/com/rabbi/authservice/dto/LoginResponseDTO.java
@@ -0,0 +1,15 @@
+package com.rabbi.authservice.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class LoginResponseDTO {
+    private final String token;
+    public LoginResponseDTO(String token) {
+        this.token = token;
+    }
+}
