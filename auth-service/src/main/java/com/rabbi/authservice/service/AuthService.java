923ed6d16979ba9bd8d84e0e1d80036428ca47ef
Fuad Rabbi <fuadhrabbi66@gmail.com>
2025-09-07 22:39:04 -0400

    created AuthService and UserService

---
+++ auth-service/src/main/java/com/rabbi/authservice/service/AuthService.java
@@ -0,0 +1,33 @@
+package com.rabbi.authservice.service;
+
+import com.rabbi.authservice.dto.LoginRequestDTO;
+import com.rabbi.authservice.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AuthService {
+    private final UserService userService;
+
+    public AuthService (UserService userService) {
+        this.userService = userService;
+    }
+    /*
+    Find the user by email.
+    If no user → fails.
+    If found, check the password.
+    If password wrong → fail.
+    If correct, → generate a token and return it.
+    */
+    public Optional<String> authenticate(LoginRequestDTO loginRequestDTO) {
+        Optional<String> token = userService.findByEmail(loginRequestDTO.getEmail())
+                .filter(u -> passwordEncoder.matches(loginRequestDTO.getPassword(),
+                        u.getPassword()))
+                .map(u -> jwtUtil.generateToken(u.getEmail(), u.getRole()));
+
+        return token;
+
+    }
+}
+//TODO: create password encoder 7,22,03
