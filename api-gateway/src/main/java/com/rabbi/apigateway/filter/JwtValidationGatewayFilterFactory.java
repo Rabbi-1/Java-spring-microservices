6e6a04b5c7da8d8cd14fd75f1ffac9b65d9b2416
Fuad Rabbi <fuadhrabbi66@gmail.com>
2025-10-12 03:12:44 -0400

    fix(gateway): resolve 500 error from JwtValidation by enforcing HTTP base URL and adding error handling
    - Reverted to stable Spring Boot 3.4.1 and Spring Cloud 2024.0.0 for compatibility
    
    - Restored gateway route config to spring.cloud.gateway.routes

--- api-gateway/src/main/java/com/rabbi/apigateway/filter/JwtValidationGatewayFilterFactory.java
+++ api-gateway/src/main/java/com/rabbi/apigateway/filter/JwtValidationGatewayFilterFactory.java
@@ -1,6 +1,5 @@
 package com.rabbi.apigateway.filter;
 
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
@@ -15,11 +14,6 @@ public class JwtValidationGatewayFilterFactory extends
 
     private final WebClient webClient;
 
-    /*
-      Create a WebClient connected to the Auth Service.
-      The base URL is read from the application configuration: auth.service.url.
-      This WebClient will be used later to call /validate on the Auth Service.
-    */
     public JwtValidationGatewayFilterFactory(WebClient.Builder webClientBuilder,
                                              @Value("${auth.service.url}") String authServiceUrl) {
         this.webClient = webClientBuilder.baseUrl(authServiceUrl).build();
@@ -28,31 +22,20 @@ public class JwtValidationGatewayFilterFactory extends
     @Override
     public GatewayFilter apply(Object config) {
         return (exchange, chain) -> {
-            // Extract the Authorization header from the incoming request
             String token =
                     exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
 
-      /*
-        If the token is missing or doesn’t begin with "Bearer ",
-        immediately stop processing and return 401 Unauthorized.
-      */
             if(token == null || !token.startsWith("Bearer ")) {
                 exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
                 return exchange.getResponse().setComplete();
             }
 
-      /*
-        Send the token to the Auth Service /validate endpoint.
-        Pass the Authorization header as-is.
-        If the response is successful → continue the filter chain (request passes through).
-        The call is non-blocking because WebClient uses reactive programming.
-      */
             return webClient.get()
                     .uri("/validate")
                     .header(HttpHeaders.AUTHORIZATION, token)
                     .retrieve()
-                    .toBodilessEntity()        // No body expected, only status code
-                    .then(chain.filter(exchange)); // Continue request if validation succeeds
+                    .toBodilessEntity()
+                    .then(chain.filter(exchange));
         };
     }
 }
